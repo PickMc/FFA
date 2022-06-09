95dbd4401f617a76c640b5860eb3003ff44b19d5
Ryan <51235797+Zxoir@users.noreply.github.com>
2022-06-09 20:54:57 +0300

    Added Kill rewards (Health, snowballs, and arrows)

--- src/main/java/me/zxoir/pickmcffa/listener/KillActionListener.java
+++ src/main/java/me/zxoir/pickmcffa/listener/KillActionListener.java
@@ -3,11 +3,15 @@ package me.zxoir.pickmcffa.listener;
 import me.zxoir.pickmcffa.PickMcFFA;
 import me.zxoir.pickmcffa.customclasses.User;
 import me.zxoir.pickmcffa.managers.ConfigManager;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * MIT License Copyright (c) 2022 Zxoir
@@ -40,6 +44,64 @@ public class KillActionListener implements Listener {
         if (killerUser.getSelectedKit() != null)
             killerUser.getSelectedKit().killAction(playerUser, killerUser);
 
+        boolean isPremiumPlus = player.hasPermission("group.premiumplus");
+        boolean isPremium = player.hasPermission("group.premium");
+        boolean isInfluencer = player.hasPermission("group.influencer");
+        ItemStack snowballs = getSnowballs(player.getInventory());
+
+        if (isPremiumPlus) {
+
+            killer.setHealth(Math.min(20, killer.getHealth() + 12));
+            if (snowballs == null)
+                player.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 24));
+            else
+                snowballs.setAmount(24);
+            player.getInventory().addItem(new ItemStack(Material.ARROW, 2));
+
+        } else if (isPremium) {
+            killer.setHealth(Math.min(20, killer.getHealth() + 10));
+            if (snowballs == null)
+                player.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 20));
+            else
+                snowballs.setAmount(20);
+            player.getInventory().addItem(new ItemStack(Material.ARROW, 2));
+        } else if (isInfluencer) {
+            killer.setHealth(Math.min(20, killer.getHealth() + 10));
+            if (snowballs == null)
+                player.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 20));
+            else
+                snowballs.setAmount(20);
+            player.getInventory().addItem(new ItemStack(Material.ARROW, 2));
+        } else {
+            killer.setHealth(Math.min(20, killer.getHealth() + 8));
+            if (snowballs == null)
+                player.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 16));
+            else
+                snowballs.setAmount(16);
+            player.getInventory().addItem(new ItemStack(Material.ARROW, 1));
+        }
+
         event.getDrops().clear();
+    }
+
+    @Nullable
+    private ItemStack getSnowballs(PlayerInventory inventory) {
+        boolean snowballsFound = false;
+        ItemStack snowballs = null;
+
+        for (ItemStack itemStack : inventory.getContents()) {
+
+            if (itemStack != null && itemStack.getType().equals(Material.SNOW_BALL)) {
+
+                if (!snowballsFound) {
+                    snowballsFound = true;
+                    snowballs = itemStack;
+                } else inventory.remove(itemStack);
+
+            }
+
+        }
+
+        return snowballs;
     }
 }
